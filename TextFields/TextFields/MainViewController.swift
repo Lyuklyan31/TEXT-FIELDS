333a777dfba16bfba0fc251693f5de898c2af8e0
oleg <lyuclyan.oleg14@gmail.com>
2024-08-05 23:29:59 +0300

    Font setting

--- TextFields/TextFields/MainViewController.swift
+++ TextFields/TextFields/MainViewController.swift
@@ -15,18 +15,28 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         
     }
     private func setupUI(){
-        
+        setupTitleLabel()
+        view.backgroundColor = .systemBackground
     }
 
     private func setupTitleLabel() {
         titleLabel.text = "Text Fields"
-        titleLabel.font = UIFont(name: "Rubik-Black", size: 34)
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: "Rubik", size: 34)
+        titleLabel.textColor = UIColor(.nightRider)
         
         view.addSubview(titleLabel)
      
+        titleLabel.snp.makeConstraints { make in
+                   make.centerX.equalToSuperview()
+                   make.top.equalTo(view.safeAreaLayoutGuide).offset(92)
+                   make.leading.trailing.equalTo(view).inset(16)
+               }
+
         
     }
 }
