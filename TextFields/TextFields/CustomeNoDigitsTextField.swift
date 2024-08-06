4b0e09d642b4c9d0a706b487f4928e31eb002ae0
oleg <lyuclyan.oleg14@gmail.com>
2024-08-06 23:51:25 +0300

    change Font

--- TextFields/TextFields/CustomeNoDigitsTextField.swift
+++ TextFields/TextFields/CustomeNoDigitsTextField.swift
@@ -1,16 +1,9 @@
-//
-//  CustomTextField.swift
-//  TextFields
-//
-//  Created by admin on 06.08.2024.
-//
-
 import UIKit
 import SnapKit
 
 class CustomeNoDigitsTextField: UIView {
     
-    private let backraund = UIView()
+    private let background = UIView()
     private let textField = UITextField()
     private let titleLabel = UILabel()
     
@@ -26,33 +19,34 @@ class CustomeNoDigitsTextField: UIView {
     
     private func setupCustomTextField() {
         titleLabel.text = "NO digits field"
-        titleLabel.font = UIFont(name: "Rubik", size: 13)
+        titleLabel.font = UIFont(name: "RubikRegular", size: 13)
         titleLabel.textColor = UIColor.nightRider
         
-        backraund.addSubview(titleLabel)
+        background.addSubview(titleLabel)
         
         titleLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(backraund.snp.top).offset(-4)
+            make.bottom.equalTo(background.snp.top).offset(-4)
+            make.leading.equalToSuperview()
         }
         
         textField.placeholder = "Type here"
-        textField.font = UIFont(name: "Rubik", size: 17)
+        textField.font = UIFont(name: "RubikRegular", size: 17)
         
-        backraund.backgroundColor = UIColor.fieldGray
-        backraund.layer.cornerRadius = 11
+        background.backgroundColor = UIColor.fieldGray
+        background.layer.cornerRadius = 11
         
-        backraund.layer.borderWidth = 1.0
-        backraund.layer.borderColor = UIColor(.fieldGray.opacity(0.12)).cgColor
+        background.layer.borderWidth = 1.0
+        background.layer.borderColor = UIColor(.fieldGray.opacity(0.12)).cgColor
         
-        self.addSubview(backraund)
+        self.addSubview(background)
         
-        backraund.snp.makeConstraints { make in
+        background.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.height.equalTo(36)
             make.leading.trailing.equalToSuperview()
         }
         
-        backraund.addSubview(textField)
+        background.addSubview(textField)
         textField.snp.makeConstraints { make in
             make.edges.equalToSuperview().inset(UIEdgeInsets(top: 7, left: 8, bottom: 7, right: 8))
         }
