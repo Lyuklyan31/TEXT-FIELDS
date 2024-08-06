ad4c76e1b223fe059eee35f68b4b538982384cb0
oleg <lyuclyan.oleg14@gmail.com>
2024-08-06 23:54:43 +0300

    addCustomePasswordTextField

---
+++ TextFields/TextFields/CustomePasswordTextField.swift
@@ -0,0 +1,13 @@
+//
+//  CustomePasswordTextField.swift
+//  TextFields
+//
+//  Created by admin on 06.08.2024.
+//
+
+import UIKit
+import SnapKit
+
+class CustomePasswordTextField: UIView {
+    
+}
